e919a8919543cdc6058b08e663c9643ec5cd5b8f
chunyeah <yechunjun@gmail.com>
2022-08-12 18:49:44 +0800

    feat: init portfolio

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, Page, Link } from '@geist-ui/react';
+import './App.css';
+
+function App() {
+
+  return (
+    <Page size='small' style={{ padding: '0 0.66rem', background: '#fff' }}>
+      <div>
+        <Text style={{ textAlign: 'left', fontSize: '30px', fontWeight: '500' }} h2>
+          chunye's blog
+        </Text>
+        <Text p>
+          Hey there! I'm Chun Ye. I'm a developer at Shanghai, China.
+        </Text>
+        <Link block target='_blank' href='https://rainbow.me/chunye.eth'>
+            chunye.eth
+        </Link>
+        <Link block color target='_blank' href='https://mirror.xyz/chunye.eth'>
+          Mirror Blog
+        </Link>
+        <Link block target='_blank' href='https://github.com/chunyeah'>
+          GitHub
+        </Link>
+        <Link block target='_blank' href='https://twitter.com/chunyeah'>
+          Twitter
+        </Link>
+      </div>
+    </Page>
+  );
+}
+
+export default App;
